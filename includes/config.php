b38d1ee2883dd991c02c01cb9512542e8e919744
Damian Joshua <147677171+DScribbler@users.noreply.github.com>
2025-10-03 13:58:02 +0100

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+// Edit these to match XAMPP MySQL settings
+define('DB_HOST','localhost');
+define('DB_NAME','phishguard');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_CHARSET','utf8mb4');
+
+// SMTP (optional)
+
+?>
+
